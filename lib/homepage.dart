5ca0c42d1e783ba0b49a9d9f47ae8cfed734c366
Kuti Gbolahan <kutigbolahan.ab@gmail.com>
2023-12-14 08:11:39 +0100

    added task

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -15,49 +15,167 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(16, 28, 46, 1),
-      appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(16, 28, 46, 1),
-        leading: const Icon(
-          Icons.person,
-          color: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: [
+                  const Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                          color: const Color.fromRGBO(24, 40, 66, 1),
+                          borderRadius: BorderRadius.circular(30)),
+                      child: const Icon(
+                        CupertinoIcons.bell,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                          color: const Color.fromRGBO(24, 40, 66, 1),
+                          borderRadius: BorderRadius.circular(30)),
+                      child: const Icon(
+                        CupertinoIcons.add,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Text('Good\nMorning',
+                  style: TextStyle(
+                      color: Color.fromRGBO(93, 139, 255, 1),
+                      fontSize: 44,
+                      fontWeight: FontWeight.bold)),
+              const SizedBox(
+                height: 10,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Today's Monday",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold)),
+                      Text('Dec 12,2023',
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("75% Done",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold)),
+                      Text('Completed task',
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 25,
+                    width: 30,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15)),
+                    child: const Center(
+                      child: Text('12',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  const Text('Tasks',
+                      style: TextStyle(
+                          color: Color.fromRGBO(93, 139, 255, 0.7),
+                          fontSize: 28,
+                          fontWeight: FontWeight.w400)),
+                  Spacer(),
+                  Container(
+                    height: 25,
+                    width: 25,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(
+                          color: Color.fromRGBO(195, 213, 248, 0.7),
+                        )),
+                    child: const Center(
+                      child: Text('3',
+                          style: TextStyle(
+                              color: Color.fromRGBO(195, 213, 248, 0.7),
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  const Text('Boards',
+                      style: TextStyle(
+                          color: Color.fromRGBO(195, 213, 248, 0.7),
+                          fontSize: 28,
+                          fontWeight: FontWeight.w400)),
+                ],
+              ),
+              const SizedBox(
+                height: 7,
+              ),
+              const SizedBox(
+                height: 2,
+                child: LinearProgressIndicator(
+                  backgroundColor: Color.fromRGBO(195, 213, 248, 0.7),
+                  value: 0.5,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
         ),
-        actions: [
-          Container(
-            height: 50,
-            width: 50,
-            decoration: BoxDecoration(
-                color: const Color.fromRGBO(24, 40, 66, 1),
-                borderRadius: BorderRadius.circular(30)),
-            child: const Icon(
-              CupertinoIcons.bell,
-              color: Colors.white,
-            ),
-          ),
-          Container(
-            height: 50,
-            width: 50,
-            decoration: BoxDecoration(
-                color: const Color.fromRGBO(24, 40, 66, 1),
-                borderRadius: BorderRadius.circular(30)),
-            child: const Icon(
-              CupertinoIcons.add,
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ),
-      body: const Column(
-        children: <Widget>[
-          Text('Good\nMorning',
-              style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 44,
-                  fontWeight: FontWeight.bold)),
-        ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue,
+        backgroundColor: const Color.fromRGBO(93, 139, 255, 1),
         onPressed: () {},
         tooltip: 'Add',
         child: const Icon(
