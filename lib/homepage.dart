cb8ec2a72034ba0072938877f640f6c01f23dc2b
Kuti Gbolahan <kutigbolahan.ab@gmail.com>
2024-01-01 08:45:36 +0100

    clean up

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 import 'package:task/homePage_details.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -328,7 +328,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 TaskWidget(
                   time: '1h 30m',
-                  color: Color.fromRGBO(156, 236, 254, 0.9),
+                  color: const Color.fromRGBO(156, 236, 254, 0.9),
                   smallText: 'Myself',
                   bigText: 'Walk My Dog',
                   image: ClipOval(
@@ -349,7 +349,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   },
                   child: TaskWidget(
                       time: '2h 45m',
-                      color: Color.fromRGBO(254, 249, 112, 1),
+                      color: const Color.fromRGBO(254, 249, 112, 1),
                       smallText: 'Sweet Home',
                       bigText: 'Grocery Shopping',
                       image: Row(
